d65e79cc12ab8bee85dadbbaeabfd5b31ba333ff
Tyler <ArKaes1820@yahoo.com>
2022-10-17 19:32:35 -0700

    Began working on the lunch decider + other misc
    Changed some fonts
    Added a github icon/link in the footer
    Added some random quotes that can be generated on the homepage
    Misc adjustments

--- js/copyright-date.js
+++ js/copyright-date.js
@@ -1,4 +1,4 @@
 var copyRightElement = document.querySelector('.copyright');
 var currentYear = new Date().getFullYear();
 
-copyRightElement.innerHTML = `&copy; ${currentYear} Tyler Montgomery`
+copyRightElement.innerHTML = `&copy; ${currentYear} Tyler Montgomery <a href='https://github.com/ArKaes13' style='text-decoration:none; color:white;'><i class="fa-brands fa-github"></a></i>`
--- js/index.js
+++ js/index.js
@@ -2,18 +2,28 @@ let foodImage = ['./images/butter-chicken.jpg', './images/shoyu-ramen-broth.jpg'
 let foodName = ['Butter Chicken', 'Shoyu Ramen Broth', 'Chicken Katsu', 'Chicken Shawarma', 'Creamy Garlic Pork Chops', 'Creamy Parmesan Chicken', 'Flat Iron Steak with Gochujang Marinade', 'Garlic Butter Pork Chops', 'Garlic Butter Shrimp', 'Jalapeno Chicken', 'Korean Short Ribs', 'Lemon Chicken', 'Mongolian Beef', 'Orange Chicken', 'Parmesan Meatballs', 'Pork Lumpia', 'Smoked Beef Short Ribs', 'Tortilla Soup', 'Vegetable Tempura', 'Wonton Soup'];
 let foodLink = ['./recipes/butter-chicken.html', './recipes/shoyu-ramen-broth.html', './recipes/chicken-katsu.html', './recipes/chicken-shawarma.html', './recipes/creamy-garlic-pork-chops.html', './recipes/creamy-parmesan-chicken.html', './recipes/flat-iron-steak-with-gochujang-marinade.html', './recipes/garlic-butter-pork-chops.html', './recipes/garlic-butter-shrimp.html', './recipes/jalapeno-chicken.html', './recipes/korean-short-ribs.html', './recipes/lemon-chicken.html', './recipes/mongolian-beef.html', './recipes/orange-chicken.html', './recipes/parmesan-meatballs.html', './recipes/pork-lumpia.html', './recipes/smoked-beef-short-ribs.html', './recipes/tortilla-soup.html', './recipes/vegetable-tempura.html', './recipes/wonton-soup.html'];
 let series = 1;
+let randomNumber = Math.random();
 
 function randomFood() {
-    var randomNumber = Math.floor(Math.random() * foodImage.length);
-    document.getElementById(`image` + `${series}`).src = foodImage[randomNumber];
-    document.getElementById(`text` + `${series}`).textContent = foodName[randomNumber];
-    document.getElementById(`link` + `${series}`).href = foodLink[randomNumber];
-    foodImage.splice(randomNumber, 1);
-    foodName.splice(randomNumber, 1);
-    foodLink.splice(randomNumber, 1);
+    var foodNumber = Math.floor(randomNumber * foodImage.length);
+    document.getElementById(`image` + `${series}`).src = foodImage[foodNumber];
+    document.getElementById(`text` + `${series}`).textContent = foodName[foodNumber];
+    document.getElementById(`link` + `${series}`).href = foodLink[foodNumber];
+    foodImage.splice(foodNumber, 1);
+    foodName.splice(foodNumber, 1);
+    foodLink.splice(foodNumber, 1);
     series++;
+}
+
+let quotes = [`Let's get cookin!`, `Classic Heather`, `Is the juice worth the squeeze?`, `Scrumdiddlyumptious!`, `Stickiest of the Icky`, `Mmm Mmm Good`, `More than good, it's gggreat!`, `Love you Heather :)`, `We Feast!`, `Food is love`]
+
+function randomQuote() {
+    var quoteNumber = Math.floor(randomNumber * quotes.length);
+    document.querySelector('.randomQuotes').textContent = quotes[quoteNumber];
 }
 
 for (i = 0; i < 4; i++) {
     randomFood();
 }
+
+randomQuote();
